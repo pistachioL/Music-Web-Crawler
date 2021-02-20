b7f3be378bf62af3872bae9c8cf82a7ab1144f41
pistachioL <785409363@qq.com>
2021-02-20 21:09:53 +0800

    酷狗音乐爬虫 first commit

---
+++ crawler/KugoMusic/src/spider/popularList.go
@@ -0,0 +1,26 @@
+package spider
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+)
+
+func parseHtml(html string) {
+	doc, err := goquery.NewDocument(html)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	doc.Find("a[class=pc_temp_songname]").Each(func(i int, selection *goquery.Selection) {
+		selection.Attr("href")
+		res:= selection.Text()
+		fmt.Println(res)
+	})
+
+
+
+}
+func main() {
+	parseHtml("https://www.kugou.com/yy/html/rank.html")
+
+}
